bfaceb0891c80fe66c6f226a41c325c9619a7f36
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-16 00:00:43 +0530

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -6,7 +6,10 @@ resource "aws_subnet" "public" {
   count = 2
   vpc_id     = aws_vpc.this.id
   cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
+
+data "aws_availability_zones" "available" {}
 
 output "vpc_id" {
   value = aws_vpc.this.id
