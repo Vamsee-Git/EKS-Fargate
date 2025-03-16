5467951a8ec22fa3b25d0cdb46e75428549ca1c4
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-16 18:08:10 +0530

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -34,10 +34,32 @@ resource "aws_iam_role_policy_attachment" "service_policy" {
   role       = aws_iam_role.this.name
 }
 
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "${var.cluster_name}-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
+
 resource "aws_eks_fargate_profile" "this" {
   cluster_name         = aws_eks_cluster.this.name
   fargate_profile_name = var.fargate_profile_name
-  pod_execution_role_arn = aws_iam_role.this.arn
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
 
   subnet_ids = var.subnet_ids
 
